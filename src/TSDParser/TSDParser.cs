1dd17b639b0054772be3bc43f5925b1a0b5887f8
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-08-25 15:50:47 -0700

    wip

--- src/TSDParser/TSDParser.cs
+++ src/TSDParser/TSDParser.cs
@@ -9,27 +9,14 @@ namespace TSDParser
 {
     public static class TSDParser
     {
-        public static Parser<string> Identifier = Parse.Identifier(Parse.Letter, Parse.LetterOrDigit);
+        public static Parser<string> Name = Parse.Identifier(Parse.Letter, Parse.LetterOrDigit);
 
-        public static Parser<string> TypeName = Parse.Identifier(Parse.AnyChar, Parse.Or(Parse.LetterOrDigit, Parse.Chars("]")));
-
-        public static Parser<Property> Property =
-            from name in Identifier.Token()
-            from nullable in Parse.Char('?').Optional()
-            from colon in Parse.Char(':').Token()
-            from type in TypeName.Token()
-            from semicolon in Parse.Char(';').Token()
-            select new Property
-            {
-                Name = name,
-                Type = type,
-                IsNullable = nullable.IsDefined
-            };
+        public static Parser<string> TypeName = Parse.Identifier(Parse.Letter, Parse.LetterOrDigit);
 
         public static Parser<Interface> Interface =
             from exp in Parse.String("export").Text().Token()
             from inter in Parse.String("interface").Text().Token()
-            from name in Identifier.Token()
+            from name in Name.Token()
             from openBrace in Parse.Char('{').Token()
             from properties in Property.Many()
             from closeBrace in Parse.Char('}').Token()
@@ -38,6 +25,21 @@ namespace TSDParser
                 Name = name,
                 Properties = properties.ToList()
             };
+
+        public static Parser<Property> Property =
+            from name in Name.Token()
+            from nullable in Parse.Char('?').Optional()
+            from colon in Parse.Char(':').Token()
+            from type in TypeName.Token()
+            from array in Parse.String("[]").Optional()
+            from semicolon in Parse.Char(';').Token()
+            select new Property
+            {
+                Name = name,
+                Type = type,
+                IsArray = array.IsDefined,
+                IsNullable = nullable.IsDefined
+            };
     }
 
     public class Property
@@ -45,6 +47,8 @@ namespace TSDParser
         public string Name { get; set; }
         public string Type { get; set; }
         public bool IsNullable { get; set; }
+        public bool IsArray { get; set; }
+
     }
 
     public class Interface : Comment
--- tests/TSDParser.Tests/UnitTest1.cs
+++ tests/TSDParser.Tests/UnitTest1.cs
@@ -18,6 +18,7 @@ export interface SomeType {
             output.Name.Should().Be("SomeType");
             output.Properties[0].Name.Should().Be("name");
             output.Properties[0].Type.Should().Be("string");
+            output.Properties[0].IsArray.Should().BeFalse();
             output.Properties[0].IsNullable.Should().BeFalse();
 
             output.Properties.Count.Should().Be(1);
@@ -37,13 +38,53 @@ export interface SomeType {
             output.Name.Should().Be("SomeType");
             output.Properties[0].Name.Should().Be("name");
             output.Properties[0].Type.Should().Be("string");
+            output.Properties[0].IsArray.Should().BeFalse();
             output.Properties[0].IsNullable.Should().BeFalse();
 
             output.Properties[1].Name.Should().Be("name2");
             output.Properties[1].Type.Should().Be("string");
+            output.Properties[1].IsArray.Should().BeFalse();
             output.Properties[1].IsNullable.Should().BeFalse();
 
             output.Properties.Count.Should().Be(2);
+        }
+
+        [Fact]
+        public void InterfaceNullableProperty()
+        {
+            var tsd = """
+export interface SomeType {
+  name?: string;
+}
+""";
+            var output = TSDParser.Interface.Parse(tsd);
+
+            output.Name.Should().Be("SomeType");
+            output.Properties[0].Name.Should().Be("name");
+            output.Properties[0].Type.Should().Be("string");
+            output.Properties[0].IsArray.Should().BeFalse();
+            output.Properties[0].IsNullable.Should().BeTrue();
+
+            output.Properties.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public void InterfaceArrayProperty()
+        {
+            var tsd = """
+export interface SomeType {
+  name: string[];
+}
+""";
+            var output = TSDParser.Interface.Parse(tsd);
+
+            output.Name.Should().Be("SomeType");
+            output.Properties[0].Name.Should().Be("name");
+            output.Properties[0].Type.Should().Be("string");
+            output.Properties[0].IsArray.Should().BeTrue();
+            output.Properties[0].IsNullable.Should().BeFalse();
+
+            output.Properties.Count.Should().Be(1);
         }
     }
 }
