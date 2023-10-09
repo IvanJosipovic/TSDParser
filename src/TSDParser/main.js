751128fac754e1ce567382961f4345c1d0819b35
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2023-10-09 12:51:28 -0700

    feat: minify js

---
+++ src/TSDParser/main.js
@@ -0,0 +1,22 @@
+import ts from "typescript";
+
+function getCircularReplacer() {
+    const ancestors = [];
+    return function (key, value) {
+        if (typeof value !== "object" || value === null) {
+            return value;
+        }
+        // `this` is the object that value is contained in,
+        // i.e., its direct parent.
+        while (ancestors.length > 0 && ancestors.at(-1) !== this) {
+            ancestors.pop();
+        }
+        if (ancestors.includes(value)) {
+            return "[Circular]";
+        }
+        ancestors.push(value);
+        return value;
+    };
+}
+
+module.exports = (callback, source) => callback(null, JSON.stringify(ts.createSourceFile("definition.d.ts", source, ts.ScriptTarget.Latest, false), getCircularReplacer()));
